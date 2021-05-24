6d6a0ac5ff9908d2a930e8e4f49c696ec370fb70
Rushik Ramani <rush.ramani@gmail.com>
2021-05-24 16:02:04 +0530

    home page styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Directry from "../../components/directory/directory.component";
+
+import './homepage.styles.scss'
+
+const HomePage = () => {
+  return (
+    <div className="homepahe">
+      <Directry />
+    </div>
+  );
+};
+
+export default HomePage;
